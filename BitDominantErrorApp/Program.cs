164ec985dc782594bd24a0c794235174a004edcd
IndigoCarmine <79552713+IndigoCarmine@users.noreply.github.com>
2023-04-18 22:22:27 +0900

    プロジェクト ファイルを追加します。

---
+++ BitDominantErrorApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Speech.Recognition;
+using System.Media;
+using System.Reflection;
+using BitDominantErrorApp.Properties;
+
+class Program
+{
+    static SoundPlayer soundPlayer;
+    static void Main()
+    {
+
+        SpeechRecognitionEngine speech = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("ja-JP"));
+
+        GrammarBuilder grammar = new();
+
+        grammar.Append("ビット");
+
+        speech.LoadGrammar(new Grammar(grammar));
+
+        speech.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognised);
+
+        speech.SetInputToDefaultAudioDevice();
+
+        speech.RecognizeAsync(RecognizeMode.Multiple);
+
+        soundPlayer = new(Resources.sound);
+
+        while (true)
+        {
+            Console.ReadLine();
+        }
+
+
+    }
+
+    static void recognised(object sender, SpeechRecognizedEventArgs arg)
+    {
+        Console.WriteLine("Eeee");
+        soundPlayer.Play();
+    }
+}
